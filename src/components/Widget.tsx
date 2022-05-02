6451b66f056c1c25d153f8394bc4a2d568b405ae
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-02 10:08:37 -0300

    feat: finalizando style do widget.

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -3,9 +3,18 @@ import { ChatTeardropDots } from 'phosphor-react'
 
 export function Widget () {
     return (
-        <button className="bg-violet-500 rounded-full px-3 h-12 text-white" >
+        // colocando o botão no fundo inferior da tela.
+        <div className="absolute bottom-5 right-5">
+        <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group" >
             {/* caso queira, basta colocar o mouse em cima do w-6 para ver o tamano em px. */}
             <ChatTeardropDots className="w-6 h-6" />
+
+            {/* definindo a largura maxima com "max-w", o overflow-hidden faz tudo o que passar do limite do width sumir. */}
+            <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                <span className="pl-2"></span>
+                Feedback
+            </span> 
         </button>
+        </div>
     )
 }
