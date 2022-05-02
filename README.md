800baf208d261cdd7a8a42e897fb5893a702548e
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-02 10:27:57 -0300

    hotfeat: usando popover ao inves de useState.

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 
 > Tailwindcss:
 - https://tailwindcss.com/docs/installation/using-postcss
+
+> headlessui
+- https://headlessui.dev/react/popover
+
+
+# O que é vite:
+- Basicamente um pacote que nos ajuda a criar um projectName, selecionar o framework (Ex: react) e a variante (ex: react-typescript). É excelente para criar novos projetos do 0, fornecendo todo o básico. 
+
+
+# O que é Tailwind:
 - Uma facilitadora de css. Integra o vite + react com carregamentos e importações.
 - Dentro do tailwind.config.js em "content" colocamos qual os tipos de arquivos que vamos trabalhar.
 - Usamos sempre className pare autilizar o tailwind.
@@ -11,6 +21,7 @@
 - Podemos criar nossas próprias cores no tailwind. Basta ir no tailwind.config.js -> extends, selecionar a propriedade color, atribuir um nome para essa cor e as tonalidades dela. Depois, basta atribuir o código hexadecimal. Usamos esse exemplo na cor "brand"
 - Outra coisa excelente é que podemos colocar qualquer coisa no de style dentro do className, visto que o tailwind interpreta o código do jeito dele. Vale muito à pena dar uma olhada na documentação deles.
 
-# O que é vite:
-- Basicamente um pacote que nos ajuda a criar um projectName, selecionar o framework (Ex: react) e a variante (ex: react-typescript). É excelente para criar novos projetos do 0, fornecendo todo o básico. 
 
+# O que é Handlessui:
+- Podemos utilizar diversos componentes importados, como menu, listbox, popover. Podendo acessar por components, como: Popover.Button
+- Uma bibliteca que simplica e MUITO a parte de useState. No commit de numero 8 do rep, da para ver o quanto de código utilizamos para fazer funcionar o useState ao clicar no botão e colocar o hello world. O comparativo do commit 8 com o 9 no Widget.tsx é absurdo.
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,27 +1,17 @@
 //o phospor-react nos permite importar diret o icone com o nome, para achar os icones é só acessar a biblioteca.
 import { ChatTeardropDots } from 'phosphor-react'
-import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 
 
 export function Widget () {
-    //dentro do array colocamos a condição que será atualizada e o valor que será atribuido junto do segundo nome, que é o que seta
-    //. isOpen(está aberto), setOpen(setar para aberto.)
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-    function toggleWidgetVisibility(){
-        //se tiver true deixa false e se tiver false deixa true. Basicamente seta o valor contrario dele.
-        setIsWidgetOpen(!isWidgetOpen)
-    }
-
     return (
         // colocando o botão no fundo inferior da tela.
-        <div className="absolute bottom-5 right-5">
+        //Popover = div Popover.button = botao popover.panel = p
+        <Popover className="absolute bottom-5 right-5">
 
-            {/* ternario que valida se isWidgetOpen é true. */}
-            {/* só estamos utilizando desse jeito pq o valor que vem depois do : no ternario comum é null. */}
-           {isWidgetOpen && <p>Hello World</p> } 
+            <Popover.Panel>Hello World</Popover.Panel>
 
-            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group" >
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group" >
             {/* caso queira, basta colocar o mouse em cima do w-6 para ver o tamano em px. Declarando que tudo dentro de button é o mesmo group para ser retuilizado o grouphover*/}
             <ChatTeardropDots className="w-6 h-6" />
 
@@ -30,7 +20,7 @@ export function Widget () {
                 <span className="pl-2"></span>
                 Feedback
             </span> 
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
