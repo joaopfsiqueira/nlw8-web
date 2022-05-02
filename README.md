3052cf10de030a65704e5cf6d2ddfcd09c6a46db
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-02 09:26:34 -0300

    feat: aplicando style no button com tailwind

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # NLW 8 Impulse
-> Anotações:
+> Vite
 - usamos o "npm create vite@latest", para pegar a ultima versão do VITE. 
 
-
+> Tailwindcss:
+- https://tailwindcss.com/docs/installation/using-postcss
+- Uma facilitadora de css. Integra o vite + react com carregamentos e importações.
+- Dentro do tailwind.config.js em "content" colocamos qual os tipos de arquivos que vamos trabalhar.
+- Usamos sempre className pare autilizar o tailwind.
+- Integra a extensão do vscode "Tailwind CSS Intellisense.
 
 # O que é vite:
 - Basicamente um pacote que nos ajuda a criar um projectName, selecionar o framework (Ex: react) e a variante (ex: react-typescript). É excelente para criar novos projetos do 0, fornecendo todo o básico. 
--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ interface ButtonProps {
 
 
 function Button(props: ButtonProps) {
-  return <button>{props.text ?? 'Default'}</button> //caso não tenha um text, deixa como "default"
+  //p-2 no className é para informar ao tailwind o padding que eu quero utilizar no elemento.
+  //Podemos usar m- para margin, e por ai vai.
+  // ou entao podemos colocar por lado, por exemplo: px (eixo x)
+  // h = height, w = width, m = margin, p = padding, b = border, d = display, entre outros.
+  return <button className="bg-violet-500 p-2 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? 'Default'}</button> //caso não tenha um text, deixa como "default"
 }
 
 function App() {
   return (
-    <div> 
+    <div className="flex gap-2"> 
       <Button text="Enviar" />
       <Button text="Ok"/>
       <Button />
