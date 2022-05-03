c11023d7c31908a18a22915c105869f1c47ac6ce
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-03 09:25:33 -0300

    feat: validando efeito onclick de um feedback.

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
+
+
 import { CloseButton } from "./CloseButton";
-
-
 //importamos as imagem 
 import bugImageUrl from "../assets/bug.svg";
 import ideaImageUrl from "../assets/idea.svg";
@@ -31,7 +32,15 @@ const feedbackTypes = {
     },
 };
 
+//criando um tipo para os feedback possiveis.
+//o keyof + typeof vai retornar só as CHAVES do objeto.
+type FeedbackType = keyof typeof feedbackTypes
+
+
 export function WidgetForm() {
+    //criando um estado para o tipo de feedback, ou certo, ele vai esperar o FeedbackType ou null.
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null >(null);
+
 
     //o calc é um jeito que o tailwind tem de calcular o tamanho total da tela e fazer algo com isso.
     // no caso, estamos pegando o tamanho da largura da tela e diminuindo 2rem, ou certo, vai esticar na tela toda com uma distancia de 2rem das pontas.
@@ -45,23 +54,31 @@ export function WidgetForm() {
 
             </header>
             
-           <div className="flex py-8 gap-2 w-full">
-                {/* o map vai percorrer o objeto feedbackTypes e vai criar um elemento por vez, ou seja, vai criar um elemento por cada feedbackType */}
-                {/* recebendo chave e valor. */}
-               { Object.entries(feedbackTypes).map(([key, value]) => {
-                   return (
-                       <button 
-                        key={key}
-                        className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none" 
-                        type="button"
-                        >
-
-                           <img src={value.image.source} alt={value.image.alt} />
-                           <span>{value.title}</span>
-                       </button>
-                   )
-               })}
-           </div>
+            {/* nesse caso, se a pessoa selecionar o tipo de feedback, ele some com o formulario. */}
+            {!feedbackType ? (
+                 <div className="flex py-8 gap-2 w-full">
+                 {/* o map vai percorrer o objeto feedbackTypes e vai criar um elemento por vez, ou seja, vai criar um elemento por cada feedbackType */}
+                 {/* recebendo chave e valor. */}
+                { Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button 
+                         key={key}
+                         className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none" 
+                         onClick={() => setFeedbackType(key as FeedbackType)} //inferindo de forma manual que a chave SEMPRE vai ser o FeedbackType
+                         // quando vamos passar um determinado valor dentro de um onclick ou ou subsmit através de uma função, precisamos fazer dela uma função callback.
+                         //igual o exemplo acima. () => função(valorpassado)
+                         type="button"
+                         >
+ 
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    )
+                })}
+            </div>
+            ) : (
+                <p>Hello World</p>
+            )}
             
             <footer>
                 {/* colocando um distanciamento do underline com offset. (propriedade css) */}
