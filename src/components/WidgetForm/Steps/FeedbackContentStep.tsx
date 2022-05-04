c72f85b35b367a058adb39a53a962de0bcaafd80
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-03 21:30:12 -0300

    feat: desabilitanto botão quanda não selecionado

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -13,7 +13,18 @@ interface FeedbackContentStepProps {
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested} : FeedbackContentStepProps) {
     const [screenshot, setScreenshot] = useState<string | null> (null)
+    const [comment, setComment] = useState('')
     
+    function handleSubmitFeedback(event: FormEvent){
+
+        event.preventDefault();
+
+
+        console.log({
+            screenshot,
+            comment
+        })
+    }
     
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     return (
@@ -36,10 +47,11 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested} 
         <CloseButton/>
         </header>
 
-        <form action="" className="my-4 w-full">
+        <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
             <textarea 
             className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
             placeholder="Conte com detalhes o que está acontecendo..."
+            onChange={event => setComment(event.target.value)}	
             />
 
 
@@ -52,7 +64,8 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested} 
 
                 <button
                     type="submit"
-                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    disabled={comment.length == 0}
+                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled: opacity-50 disabled:hover:bg-brand-500"
                 >
                     Enviar Feedback
                 </button>
