1929901cfb051a5931135a9eee7bccf8a36ce644
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-06 20:19:39 -0300

    feat: desativando botão enquanto estiver carregando

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -79,7 +79,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
 
                 <button
                     type="submit"
-                    disabled={comment.length == 0}
+                    disabled={comment.length == 0 || isSendingFeedback}
                     className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                 >
                     {isSendingFeedback ?  <Loading/> : 'Enviar Feedback'}
