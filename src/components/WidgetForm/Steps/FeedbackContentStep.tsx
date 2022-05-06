726aef895cdaa41ba2e5cdf6b45de002db59bcff
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-06 20:19:16 -0300

    feat: configurando loading no enviar feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -81,7 +82,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
                     disabled={comment.length == 0}
                     className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                 >
-                    Enviar Feedback
+                    {isSendingFeedback ?  <Loading/> : 'Enviar Feedback'}
                 </button>
             </footer>
         </form>
